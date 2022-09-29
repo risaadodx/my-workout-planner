62ef7f3aece617291755f15be73da231de76e656
risaadodx <wrisaad@gmail.com>
2022-09-29 14:30:21 +0600

    activity components added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Main.css";
 import Workout from "../Workout/Workout";
+import Activity from "../activity/Activity";
 
 const Main = () => {
   const [workouts, setWorkouts] = useState([]);
@@ -22,34 +23,7 @@ const Main = () => {
         </div>
       </div>
       <div className="activity-container">
-        <div className="profile">
-          <h3>Khaled Bin Wahid</h3>
-          <p>Chittagong, Bangladesh</p>
-          <div className="profile-info">
-            <div>
-              <h5>60kg</h5>
-              <p>Weight</p>
-            </div>
-            <div>
-              <h5>5.10</h5>
-              <p>Height</p>
-            </div>
-            <div>
-              <h5>30</h5>
-              <p>Age</p>
-            </div>
-          </div>
-          <div className="add-break">
-            <h3>Add A Break</h3>
-            <div className="add-break-info">
-              <div className="break-time">10s</div>
-              <div className="break-time">10s</div>
-              <div className="break-time">10s</div>
-              <div className="break-time">10s</div>
-              <div className="break-time">10s</div>
-            </div>
-          </div>
-        </div>
+        <Activity></Activity>
       </div>
     </div>
   );
---
+++ src/components/activity/Activity.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Activity.css";
+const Activity = () => {
+  return (
+    <div className="profile">
+      <h3>Khaled Bin Wahid</h3>
+      <p>Chittagong, Bangladesh</p>
+      <div className="profile-info">
+        <div>
+          <h5>60kg</h5>
+          <p>Weight</p>
+        </div>
+        <div>
+          <h5>5.10</h5>
+          <p>Height</p>
+        </div>
+        <div>
+          <h5>30</h5>
+          <p>Age</p>
+        </div>
+      </div>
+      <div className="add-break">
+        <h3>Add A Break</h3>
+        <div className="add-break-info">
+          <div className="break-time">10s</div>
+          <div className="break-time">10s</div>
+          <div className="break-time">10s</div>
+          <div className="break-time">10s</div>
+          <div className="break-time">10s</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Activity;
