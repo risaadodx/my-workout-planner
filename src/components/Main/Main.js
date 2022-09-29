a4fd6e9dcbcce1cfb0dd84351acf1b61cbe7f331
risaadodx <wrisaad@gmail.com>
2022-09-29 14:05:57 +0600

    profile info and break time added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -14,7 +14,7 @@ const Main = () => {
   return (
     <div className="main-container">
       <div className="workout-container">
-        <h4>Select Today's Workouts:{workouts.length}</h4>
+        <h4>Select Today's Workouts:</h4>
         <div className="workout-card-container">
           {workouts.map((workout) => (
             <Workout key={workout.id} workout={workout}></Workout>
@@ -22,7 +22,34 @@ const Main = () => {
         </div>
       </div>
       <div className="activity-container">
-        <h4>Activity</h4>
+        <div className="profile">
+          <h3>Khaled Bin Wahid</h3>
+          <p>Chittagong, Bangladesh</p>
+          <div className="profile-info">
+            <div>
+              <h5>60kg</h5>
+              <p>Weight</p>
+            </div>
+            <div>
+              <h5>5.10</h5>
+              <p>Height</p>
+            </div>
+            <div>
+              <h5>30</h5>
+              <p>Age</p>
+            </div>
+          </div>
+          <div className="add-break">
+            <h3>Add A Break</h3>
+            <div className="add-break-info">
+              <div className="break-time">10s</div>
+              <div className="break-time">10s</div>
+              <div className="break-time">10s</div>
+              <div className="break-time">10s</div>
+              <div className="break-time">10s</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
